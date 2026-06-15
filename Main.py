b0ec9c1fe2d21f9e9cb7fa188dbf0e612474a3be
Hengky Ramadhano <hengkyr@Hengkys-MacBook-Pro.local>
2026-06-15 21:49:47 +0700

    fix ordering the store

--- Main.py
+++ Main.py
@@ -21,21 +21,6 @@ def hit_api(sku) :
       "query": "mutation AllCheckoutOptions($input: CheckoutRequestInput!) {\n  getCheckoutOptions(requestBody: $input) {\n    online {\n      text\n      stores {\n        ...storesInfo\n        __typename\n      }\n      type\n      __typename\n    }\n    }\n}\n\nfragment storesInfo on CheckoutStore {\n  name\n  options {\n    skuList {\n      id\n            stockValue\n      price\n      subtotal\n     }\n   }\n  }"
     })
 
-    # payloadRemindMe = json.dumps([
-    #   {
-    #     "operationName": "AddProductSKUToRemindMe",
-    #     "variables": {
-    #       "requestBody": {
-    #         "branchIds": [
-    #           "jz23mo"
-    #         ],
-    #         "email": "hengkyramadhano@gmail.com",
-    #         "id": sku
-    #       }
-    #     },
-    #     "query": "mutation AddProductSKUToRemindMe($requestBody: ProductSkuAddProductSKUToRemindMeInput!) {\n  addProductSKUToRemindMe(requestBody: $requestBody) {\n    id\n    isWatched\n    isActive\n    stocks {\n      branchId\n      isReminded\n      __typename\n    }\n    __typename\n  }\n}"
-    #   }
-    # ])
     headers = {
       'content-type': 'application/json'
     }
@@ -43,23 +28,42 @@ def hit_api(sku) :
     response = requests.request("POST", url, headers=headers, data=payload)
     json_response = json.loads(response.text)
 
-    jumlah_data = len(json_response["data"]["getCheckoutOptions"]["online"]["stores"][0]["options"][0]["skuList"])
+    urutan_toko = [
+      "Gudang Online",
+      "Toko Jakarta Pusat",
+      "Toko Jakarta Barat",
+      "Toko Jakarta Utara",
+      "Toko Tangerang",
+      "Toko Cikupa"
+    ]
+
+    data = json_response["data"]["getCheckoutOptions"]["online"]
+    jumlah_data = len(json_response["data"]["getCheckoutOptions"]["online"]["stores"])
+
+    # Buat dictionary: nama_toko -> data store
+    store_map = {store["name"]: store for store in data["stores"]}
    
     data_list = []
     for item in range(jumlah_data):
       data_row = {}
-      for stockItem in range(6):  # Jumlah dari berapa banyak cabang yang di ambil
-        nm_lokasi = json_response["data"]["getCheckoutOptions"]["online"]["stores"][stockItem]["name"]
-        skuId = json_response["data"]["getCheckoutOptions"]["online"]["stores"][stockItem]["options"][0]["skuList"][item]["id"]
-        stock = json_response["data"]["getCheckoutOptions"]["online"]["stores"][stockItem]["options"][0]["skuList"][item]["stockValue"]
-        price = json_response["data"]["getCheckoutOptions"]["online"]["stores"][stockItem]["options"][0]["skuList"][item]["price"]
+
+      for nm_toko in urutan_toko:
+        # Skip jika toko tidak ada di response
+        if nm_toko not in store_map:
+            continue
+
+        stores = store_map[nm_toko]
+
+        skuId = stores["options"][0]["skuList"][item]["id"]
+        stock = stores["options"][0]["skuList"][item]["stockValue"]
+        price = stores["options"][0]["skuList"][item]["price"]
       
         if stock == 999999:
            stock = 100
         
         sku_detail = {"SKU" : skuId}
         price_final = {"Price" : hitung_harga(price)}
-        product_name_dict = {nm_lokasi : stock}
+        product_name_dict = {nm_toko : stock}
         data_row.update(sku_detail)
         data_row.update(price_final)
         data_row.update(product_name_dict)
@@ -78,42 +82,48 @@ def update_progress(progress):
     sys.stdout.write(f'\rProgress: [{bar}] {int(progress * 100)}%')
     sys.stdout.flush()
 
+import math
+
 def hitung_harga(B2):
-    # Menghitung D2 berdasarkan skema harga
-    if B2 < 5000:
-        D2 = B2 + 3800
-    elif B2 < 10000:
-        D2 = B2 + 6500
-    elif B2 < 20000:
-        D2 = B2 + 2000 + (B2 * 0.52)
-    elif B2 < 35000:
-        D2 = B2 + (B2 * 0.50)
-    elif B2 < 50000:
-        D2 = B2 + (B2 * 0.43)
-    elif B2 < 70000:
-        D2 = B2 + (B2 * 0.37)
-    elif B2 < 100000:
-        D2 = B2 + (B2 * 0.28)
-    elif B2 < 150000:
-        D2 = B2 + (B2 * 0.25)
-    elif B2 < 200000:
-        D2 = B2 + (B2 * 0.20)
-    elif B2 < 250000:
-        D2 = B2 + (B2 * 0.20)
-    elif B2 < 300000:
-        D2 = B2 + (B2 * 0.20)
-    elif B2 < 500000:
-        D2 = B2 + (B2 * 0.20)
-    else:
-        D2 = B2  # Jika lebih dari 500000, tidak ada tambahan dalam rumus
 
-    # Menghitung E2 (0.14 adalah komisi MP), (1400 adalah biaya pemrosesan mp)
-    E2 = D2 * 0.14 + D2 + 1400
+  # Menentukan profit berdasarkan modal
+  if B2 < 5000:
+      profit = 3800
+  elif B2 < 10000:
+      profit = 6500
+  elif B2 < 20000:
+      profit = 2000 + (B2 * 0.52)
+  elif B2 < 35000:
+      profit = B2 * 0.50
+  elif B2 < 50000:
+      profit = B2 * 0.43
+  elif B2 < 70000:
+      profit = B2 * 0.37
+  elif B2 < 100000:
+      profit = B2 * 0.28
+  elif B2 < 150000:
+      profit = B2 * 0.25
+  elif B2 < 200000:
+      profit = B2 * 0.20
+  elif B2 < 250000:
+      profit = B2 * 0.20
+  elif B2 < 300000:
+      profit = B2 * 0.20
+  elif B2 < 500000:
+      profit = B2 * 0.20
+  else:
+      profit = 0
 
-    # Menghitung F2 dengan ROUNDUP ke ratusan terdekat
-    F2 = math.ceil(E2 / 100) * 100
+  # Uang bersih yang ingin diterima
+  target_bersih = B2 + profit
 
-    return F2
+  # Menghitung harga jual marketplace
+  harga_mp = (target_bersih + 1250) / 0.80
+
+  # Pembulatan ke ratusan
+  harga_final = math.ceil(harga_mp / 100) * 100
+  
+  return harga_final
 
 # export to excel
 def export_to_excel():
@@ -132,14 +142,12 @@ SKU_Store = []
 file_name = ""
 
 if len(sys.argv) > 1:  # Memastikan ada argumen yang diberikan
-  if sys.argv[1] not in ("tiktok", "netlook", "shopee", "tourisme", "bestever", "sample"): # Nama file sku
+  if sys.argv[1] not in ("tiktok"): # Nama file sku
     print("Load Data SKU..")
-    for sku_item in range(1 , len(sys.argv)):
-      sku_terminal = sys.argv[sku_item]
-      
-      data = hit_api(sku_terminal)
-      # SKU_Store.append(data)
-      print(json.dumps(data, indent=4))
+    sku_list = sys.argv[1:]
+    data = hit_api(sku_list)
+    print(json.dumps(data, indent=4))
+
   else:
     print("Masuk ke import file")
     
@@ -147,7 +155,7 @@ if len(sys.argv) > 1:  # Memastikan ada argumen yang diberikan
     module = importlib.import_module(file_name)
     mylist = getattr(module, 'mylist')
 
-    batches = split_list(mylist, 100)
+    batches = split_list(mylist, 100) #Batches ini menampung slit data tiap 100 sku
     print(f"Total SKU: {len(mylist)}")
     jumlahSKU = len(batches)
     
